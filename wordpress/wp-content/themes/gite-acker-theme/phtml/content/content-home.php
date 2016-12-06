21ac4183b917d18cef0c87bb068a54cd697e705c
thomasbatt <thomasbatt@gmail.com>
2016-12-06 14:56:30 +0100

    firts structur inte

--- wordpress/wp-content/themes/gite-acker-theme/phtml/content/content-home.php
+++ wordpress/wp-content/themes/gite-acker-theme/phtml/content/content-home.php
@@ -1,18 +1,49 @@
 <div id="page">
-	<div class="container">
-	  <section class="background">
+	<div class="pagecontainer">
+	  <section id="accueil" class="background">
 	    <div class="content-wrapper">
 	      <p class="content-title">GITES ACKER</p>
 	      <p class="content-subtitle">Labellisé Gîtes de France depuis 2003</p>
 	    </div>
 	  </section>
-	  <section class="background">
+	  <section id="about" class="background">
 	    <div class="content-wrapper">
-	      <p class="content-title">Description</p>
-	      <p class="content-subtitle">Gîte aménagé à l'étage de la maison du propriétaire située en périphérie de village. 1 ch (3 lits 1p), séjour avec accès direct sur balcon couvert (1 lit 2p, prise TV), petite cuisine (micro-ondes), salle de bain avec wc. Chauffage central inclus. Location de draps. Présence du G714 sur la propriété. En commun : Lave-linge au sous-sol, jardin, mobilier de jardin, bac à sable, barbecue, ping-pong, parking dans cour. Supplément animaux: 5€/jour/animal.</p>
-	    </div>
+			<p class="content-title">Description</p>
+			<div class="content-subtitle text-justify">
+				<p>Gîte aménagé à l'étage de la maison du propriétaire située en périphérie de village. 1 ch (3 lits 1p), séjour avec accès direct sur balcon couvert (1 lit 2p, prise TV), petite cuisine (micro-ondes), salle de bain avec wc. Chauffage central inclus. Location de draps. Présence du G714 sur la propriété. En commun : Lave-linge au sous-sol, jardin, mobilier de jardin, bac à sable, barbecue, ping-pong, parking dans cour. Supplément animaux: 5€/jour/animal.</p>
+	    		<div class="wrapper-images row ">
+					<?php
+				    $actus = get_posts(array(
+				    	"category"=> 1,
+				    	'posts_per_page'   => 30
+				    ));
+				    ?>
+					<?php if (have_posts()) : ?>
+		                <?php 
+		                    foreach($actus as $post) :
+					    	setup_postdata($post); 
+		                ?>
+		                      
+		                <article class="col-xs-6 col-sm-3 col-md-2">  
+		                    <div class="post">
+		                    	<div class="img-wrapper">
+		                    		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+		                        </div>
+		                      </div>
+		                </article>
+		                <?php 	endforeach;
+		                wp_reset_postdata(); ?>
+
+		            <?php else : ?>
+		                <p class="nothing">
+		                    Aucune post à afficher !
+		                </p>
+		            <?php endif; ?>
+				</div>
+    		</div>
+    	</div>
 	  </section>
-	  <section class="background">
+	  <section id="contact" class="background">
 	    <div class="content-wrapper">
 	      <p class="content-title">Information propriétaire</p>
 	      <p class="content-subtitle">
--- wordpress/wp-content/themes/gite-acker-theme/phtml/layout/footer.php
+++ wordpress/wp-content/themes/gite-acker-theme/phtml/layout/footer.php
@@ -1,5 +1,10 @@
 <footer class="footer <?= $content['bg-footer'] ?>">
-	
+	<section>
+		<span>© Copyright 2016 
+			<a href="http://www.thomasbatt.fr" target="_blank">Thomas BATT</a> & 
+			<a href="http://www.faress.webatt.fr" target="_blank">Faress Hank</a>
+		</span>
+	</section>
 </footer>
 
 <div class="wp-footer">

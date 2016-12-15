834f4a8c7ce14329a76e7f55282d0a9471587a57
faresshank <fafetnach@gmail.com>
2016-12-15 12:14:36 +0100

    upgrade

--- wordpress/wp-content/themes/gite-acker-theme/phtml/skel.php
+++ wordpress/wp-content/themes/gite-acker-theme/phtml/skel.php
@@ -198,8 +198,8 @@ html {
 </style>
 
 		
-		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/vendors.min.css?v=1b64d717d1b0'/>
-		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/website.min.css?v=1b64d717d1b0'/>
+		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/vendors.min.css?v=1b76099f6d30'/>
+		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/website.min.css?v=1b76099f6d30'/>
 	</head>
 
 	<body>
@@ -210,6 +210,6 @@ html {
 	</body>
 
 	
-	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/vendors.min.js?v=1b64d717d1b0'></script>
-	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/website.min.js?v=1b64d717d1b0'></script>
+	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/vendors.min.js?v=1b76099f6d30'></script>
+	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/website.min.js?v=1b76099f6d30'></script>
 </html>

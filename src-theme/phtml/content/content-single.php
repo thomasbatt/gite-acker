e4c25aa37e5d74748ba6148cfe9f5411d7a6559b
faresshank <fafetnach@gmail.com>
2016-12-17 08:59:52 +0100

    Bg SinglePage + Width Logo home + Responsive

--- src-theme/phtml/content/content-single.php
+++ src-theme/phtml/content/content-single.php
@@ -6,6 +6,7 @@
 			    <?php if (have_posts()) : ?>
 			        <?php while (have_posts()) : the_post(); ?>
 			            <div class="post">
+			            	<div class="bg"></div>
 			                <?php the_post_thumbnail('large'); ?>
 			            </div>
 			        <?php endwhile; ?>
